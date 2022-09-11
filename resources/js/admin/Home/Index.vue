ea566f32d2522fe76cf2c26d6e82c8520c1eb5af
datprs <datprs@gmail.com>
2022-09-12 00:35:09 +0700

    Add Pinia

--- resources/js/admin/Home/Index.vue
+++ resources/js/admin/Home/Index.vue
@@ -1,3 +1,15 @@
 <template>
     <h1>Home</h1>
+    <h1>{{ count }}</h1>
+    <button @click="increment"></button>
 </template>
+
+<script setup>
+import { useUserStore } from "@/stores/user";
+import { storeToRefs } from "pinia";
+const store = useUserStore();
+
+const { count } = storeToRefs(store);
+
+const { increment } = store;
+</script>
