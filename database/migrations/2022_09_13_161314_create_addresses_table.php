67955be9f16c51ef93b94b39129345f83c66553e
datprs <datprs@gmail.com>
2022-09-15 17:15:28 +0700

    Add Migration

---
+++ database/migrations/2022_09_13_161314_create_addresses_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address1');
+            $table->string('address2')->nullable();
+            $table->string('city');
+            $table->string('ward');
+            $table->string('district');
+            $table->string('phone');
+            $table->string('email');
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
