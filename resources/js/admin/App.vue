be5bb4c71d6659430dc9c9892d91682259a6734a
datprs <datprs@gmail.com>
2022-09-15 23:09:44 +0700

    Add AdminLTE

--- resources/js/admin/App.vue
+++ resources/js/admin/App.vue
@@ -1,3 +1,5 @@
 <template>
     <router-view />
 </template>
+
+<style src="admin-lte/dist/css/adminlte.css"></style>
--- resources/js/admin/Home/Index.vue
+++ resources/js/admin/Home/Index.vue
@@ -1,7 +1,21 @@
 <template>
-    <h1>Home</h1>
-    <h1>{{ count }}</h1>
-    <button @click="increment"></button>
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Default Card Example</h3>
+            <div class="card-tools">
+                <!-- Buttons, labels, and many other things can be placed here! -->
+                <!-- Here is a label for example -->
+                <span class="badge badge-primary">Label</span>
+            </div>
+            <!-- /.card-tools -->
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">The body of the card</div>
+        <!-- /.card-body -->
+        <div class="card-footer">The footer of the card</div>
+        <!-- /.card-footer -->
+    </div>
+    <!-- /.card -->
 </template>
 
 <script setup>
